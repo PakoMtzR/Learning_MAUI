bdc7c8d53781e0f3afbea64590fd663353489a89
PakoMtzR <francisco.2k1@gmail.com>
2025-03-05 23:34:05 -0600

    Agregar archivos de proyecto.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Me tocaste {count} vez";
+            else
+                CounterBtn.Text = $"Me tocaste {count} veces";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void ResetBtn_Clicked(object sender, EventArgs e)
+        {
+            count = 0;
+            CounterBtn.Text = $"Tocame";
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ MauiApp3 - Calculator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp3___Calculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
